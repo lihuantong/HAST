bf318b59fb568cf54caf304a9b07087e36f1fc20
SccVlsi <scc_vlsi@126.com>
2023-03-10 21:08:14 +0800

    10,Mar

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 CUDA_VISIBLE_DEVICES=5 python -m torch.distributed.launch --nproc_per_node 1 main_direct.py --conf_path ./config/cifar10_resnet20.hocon
+
+CUDA_VISIBLE_DEVICES=5,6,7,8 python -m torch.distributed.launch --nproc_per_node 4 main_direct.py --conf_path ./config/cifar10_resnet20.hocon
